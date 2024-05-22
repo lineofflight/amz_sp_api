501dc114bf539520672df34f6f704afa5a1978ca
Hakan Ensari <me@hakanensari.com>
2024-05-22 18:31:17 +0200

    feat: Add grantless operations

--- lib/sp_api_client.rb
+++ lib/sp_api_client.rb
@@ -3,14 +3,18 @@ require 'api_client'
 
 module AmzSpApi
   class SpApiClient < ApiClient
-
     def initialize(config = SpConfiguration.default)
+      @grantless = false
       super(config)
     end
 
-    alias_method :super_call_api, :call_api
+    alias super_call_api call_api
     def call_api(http_method, path, opts = {})
       super(http_method, path, opts.merge(header_params: auth_headers.merge(opts[:header_params] || {})))
+    end
+
+    def grantless
+      dup.tap { |c| c.instance_variable_set(:@grantless, true) }
     end
 
     private
@@ -20,7 +24,7 @@ module AmzSpApi
       stored_token = config.get_access_token.call(config.access_token_key)
       if stored_token.nil?
         new_token = request_lwa_access_token
-        config.save_access_token.call(config.access_token_key, new_token) if config.save_access_token
+        config.save_access_token&.call(config.access_token_key, new_token)
         return new_token[:access_token]
       else
         return stored_token
@@ -33,16 +37,11 @@ module AmzSpApi
       newself.config.host = 'api.amazon.com'
 
       data, status_code, headers = newself.super_call_api(:POST, '/auth/o2/token',
-        :header_params => {
-         'Content-Type' => 'application/x-www-form-urlencoded'
+        header_params: {
+          'Content-Type' => 'application/x-www-form-urlencoded'
         },
-        :form_params =>  {
-          grant_type: 'refresh_token',
-          refresh_token: config.refresh_token,
-          client_id: config.client_id,
-          client_secret: config.client_secret
-        },
-        :return_type => 'Object')
+        form_params:  lwa_access_token_params,
+        return_type: 'Object')
 
       unless data && data[:access_token]
         fail ApiError.new(:code => status_code,
@@ -51,6 +50,24 @@ module AmzSpApi
       end
 
       data
+    end
+
+    def lwa_access_token_params
+      if @grantless
+        {
+          grant_type: 'client_credentials',
+          scope: 'sellingpartnerapi::notifications',
+          client_id: config.client_id,
+          client_secret: config.client_secret
+        }
+      else
+        {
+          grant_type: 'refresh_token',
+          refresh_token: config.refresh_token,
+          client_id: config.client_id,
+          client_secret: config.client_secret
+        }
+      end
     end
 
     def auth_headers

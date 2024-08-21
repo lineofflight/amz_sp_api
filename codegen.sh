4263224bac8e795755a0634f8d6f559c9539d594
Hakan Ensari <hakanensari@gmail.com>
2024-08-21 10:44:11 +0200

    Handle missing dirs

--- codegen.sh
+++ codegen.sh
@@ -3,12 +3,12 @@
 # exit on error
 set -e
 
-for FILE in `find ../selling-partner-api-models/models -name "*.json"`; do 
+for FILE in `find ../selling-partner-api-models/models -name "*.json"`; do
 	API_NAME=`echo $FILE | awk -F/ '{print $4}'`
 	MODULE_NAME=`echo $API_NAME | perl -pe 's/(^|-)./uc($&)/ge;s/-//g'`
 
-	rm -r lib/${API_NAME}
-	mkdir lib/$API_NAME
+	rm -rf lib/${API_NAME}
+	mkdir -p lib/$API_NAME
 	cp config.json lib/$API_NAME
 	sed -i '' "s/GEMNAME/${API_NAME}/g" lib/${API_NAME}/config.json
 	sed -i '' "s/MODULENAME/${MODULE_NAME}/g" lib/${API_NAME}/config.json
